fadeca90f648608828af141bbd2a0beb10a244d5
AlexJOYZ <zte19062015@gmail.com>
2025-08-31 23:28:09 +0800

    refactor(App.jsx) create const route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 // import '..//styles/App.css'
-import './styles/App.css'
+import './styles/App.css';
 
-
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useQuery } from './hooks';
-
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -24,26 +22,28 @@ import {
 
 import { Layout } from './components/Layout/Layout';
 import { Spinner } from './components/UI/spinner/Spinner';
+import { ROUTES } from './const';
 
 export const App = () => {
+  const isAuth = useSelector((state)=>state.account.isAuth)
   const dispatch = useDispatch();
 
-  const { isLoading, data, error } = useQuery('checkAuth', () => dispatch(checkAuth()));
+  const { isLoading, data, error } = useQuery(isAuth, () => dispatch(checkAuth()));
 
   if (isLoading) return <Spinner />;
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path={ROUTES.MAIN} element={<Layout />}>
           <Route index element={<MainPage />} />
-          <Route path='categories' element={<Categories />} />
-          <Route path='favorites' element={<Favorites />} />
-          <Route path='about' element={<About />} />
-          <Route path='contacts' element={<Contacts />} />
-          <Route path='vacancy' element={<Vacancy />} />
-          <Route path='orders' element={<Orders />} />
-          <Route path='cart' element={<Cart />} />
+          <Route path={ROUTES.CATEGORIES} element={<Categories />} />
+          <Route path={ROUTES.FAVORITES} element={<Favorites />} />
+          <Route path={ROUTES.ABOUT} element={<About />} />
+          <Route path={ROUTES.CONTACTS} element={<Contacts />} />
+          <Route path={ROUTES.VACANCY} element={<Vacancy />} />
+          <Route path={ROUTES.ORDERS} element={<Orders />} />
+          <Route path={ROUTES.CART} element={<Cart />} />
           <Route path='*' element={<ErrorPage />} />
         </Route>
       </Routes>
---
+++ src/const/route.js
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  MAIN: '/',
+  CATEGORIES: '/categories',
+  ABOUT: '/about',
+  FAVORITES: '/favorites',
+  CONTACTS: '/contacts',
+  VACANCY: '/vacancy',
+  ORDERS:'/orders',
+  CART:'/cart'
+};
