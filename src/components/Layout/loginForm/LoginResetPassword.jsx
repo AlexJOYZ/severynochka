4ecdab678043a70b1bead521cd841bfb4a21ead1
AlexJOYZ <zte19062015@gmail.com>
2025-10-29 20:30:20 +0800

    feat(LoginResetPassword.jsx): finish reset password logic

--- src/components/Layout/loginForm/LoginResetPassword.jsx
+++ src/components/Layout/loginForm/LoginResetPassword.jsx
@@ -1,52 +1,53 @@
-import ReactInputMask from 'react-input-mask';
-import { useForm } from '../../../hooks';
+import { useForm, useMutation } from '../../../hooks';
 
-import {
-  passwordValidationSchema,
-  resetPasswordFormValidateSchemes,
-  telephoneValidateSchema,
-} from '../../../utils';
-import { Input } from '../../UI/fields/Input/Input';
-import { Tooltip } from '../../UI/tooltip/Tooltip';
+import { resetPasswordFormValidateSchemes } from '../../../utils';
 
 import { Typography } from '../../UI/Typography/Typography';
 import { useState } from 'react';
 import { FillResetData } from './resetFormSteps/FillResetData';
 import { ConfirmPhoneCode } from './resetFormSteps/ConfirmPhoneCode';
+import { AuthService } from '../../../API/entities/auth';
+import { Spinner } from '../../UI/spinner/Spinner';
 
 export const LoginResetPassword = ({ setStage }) => {
   const [step, setStep] = useState('fillUserData');
+
+  const {
+    mutate: resetPasswordMutation,
+    isLoading,
+    error,
+  } = useMutation('resetPassword', (body) => AuthService.resetPassword(body), {
+    onSuccess: (response) => setStage('login'),
+    onFailure: (e) => functions.setFieldsErrors('phoneCode', e?.response?.data?.message),
+  });
 
   const { state, functions } = useForm({
     initialValues: {
       telephone: '',
       password: '',
       confirmPassword: '',
+      phoneCode: '',
     },
     validateSchema: resetPasswordFormValidateSchemes,
-    onChange: false,
-    onSubmit: () => {
-      console.log('@reset password');
-    },
+    onSubmit: () => resetPasswordMutation(state.values),
   });
- 
 
   return (
-    <form className='login__form'>
+    <form className='login__form' onSubmit={functions.handleSubmit}>
       <Typography as='h2' variant='header' size='s'>
         Новый пароль
       </Typography>
       {step === 'fillUserData' && (
-        <FillResetData state={state} functions={functions} setStep={setStep} />
+        <FillResetData state={state} functions={functions} setStep={setStep} setStage={setStage} />
       )}
       {step === 'confirmPhoneCode' && (
         <ConfirmPhoneCode
           state={state}
           functions={functions}
-          setStage={setStage}
           setStep={setStep}
         />
       )}
+      {isLoading && <Spinner />}
     </form>
   );
 };
