d5bcd3e7ae4e6b0db7e959e635175c15adc6beee
AlexJOYZ <zte19062015@gmail.com>
2025-10-30 20:51:57 +0800

    feat(Menu.jsx, categories.js): move the category into a separate constant

--- src/components/UI/menu/Menu.jsx
+++ src/components/UI/menu/Menu.jsx
@@ -4,28 +4,15 @@ import styles from './Menu.module.css';
 
 import { Link } from 'react-router-dom';
 import { Typography } from '../Typography/Typography';
+import { CATEGORIES } from '../../../const/categories';
 
 export const Menu = forwardRef(function (props, ref) {
-  const menuItems = [
-    { id: 1, title: 'Молоко, сыр, яйцо', path: 'milk' },
-    { id: 2, title: 'Хлеб', path: 'bread' },
-    { id: 3, title: 'Фрукты и овощи', path: 'vegetables' },
-    { id: 4, title: 'Замороженные продукты', path: 'frozen' },
-    { id: 5, title: 'Напитки', path: 'drinks' },
-    { id: 6, title: 'Кондитерские изделия', path: 'confectionery' },
-    { id: 7, title: 'Чай, кофе', path: 'tea' },
-    { id: 8, title: 'Бакалея', path: 'grocery' },
-    { id: 9, title: 'Здоровое питание', path: 'healthy_food' },
-    { id: 10, title: 'Зоотовары', path: 'pet_supplies' },
-    { id: 11, title: 'Непродовольственные товары', path: 'nonfood_products' },
-    { id: 12, title: 'Детское питание', path: 'formula' },
-    { id: 13, title: 'Мясо, птица, колбаса', path: 'meat' },
-  ];
+  
 
   return (
     <menu className={`container ${styles.menu}`} ref={ref}>
-      {menuItems.map((item) => (
-        <Link key={item.id} to={`/categories/catalog/${item.path}`}>
+      {CATEGORIES.map((item) => (
+        <Link key={item.id} to={`/categories/catalog/${item.id}`}>
           <Typography as='h2' variant='text-bold' size='s'>
             {item.title}
           </Typography>
---
+++ src/const/categories.js
@@ -0,0 +1,53 @@
+import milkIcon from './../assets/icons/categories/category-milk.png';
+import breadIcon from '../assets/icons/categories/category-bread.png';
+import vegetablesIcon from '../assets/icons/categories/category-vegetables.png';
+import frozenIcon from '../assets/icons/categories/category-frozen.png';
+import drinksIcon from '../assets/icons/categories/category-drinks.png';
+import confectioneryIcon from '../assets/icons/categories/category-confectionery.png';
+import teaIcon from '../assets/icons/categories/category-tea.png';
+import groceryIcon from '../assets/icons/categories/category-grocery.png';
+import healthyIcon from '../assets/icons/categories/category-healthy.png';
+import petsIcon from '../assets/icons/categories/category-pets.png';
+import formulaIcon from '../assets/icons/categories/category-formula.png';
+import meatIcon from '../assets/icons/categories/category-meat.png';
+import nonfoodIcon from '../assets/icons/categories/category-nonfood.png';
+
+export const CATEGORIES = [
+  {
+    id: 1,
+    title: 'Молоко, сыр, яйцо',
+    iconPath: milkIcon,
+  },
+  { id: 2, title: 'Хлеб', iconPath: breadIcon },
+  {
+    id: 3,
+    title: 'Фрукты и овощи',
+    iconPath: vegetablesIcon,
+  },
+  {
+    id: 4,
+    title: 'Замороженные продукты',
+    iconPath: frozenIcon,
+  },
+  { id: 5, title: 'Напитки', iconPath: drinksIcon },
+  {
+    id: 6,
+    title: 'Кондитерские изделия',
+    iconPath: confectioneryIcon,
+  },
+  { id: 7, title: 'Чай, кофе', iconPath: teaIcon },
+  { id: 8, title: 'Бакалея', iconPath: groceryIcon },
+  { id: 9, title: 'Здоровое питание', iconPath: healthyIcon },
+  { id: 10, title: 'Зоотовары', iconPath: petsIcon },
+  { id: 11, title: 'Детское питание', iconPath: formulaIcon },
+  {
+    id: 12,
+    title: 'Мясо, птица, колбаса',
+    iconPath: meatIcon,
+  },
+  {
+    id: 13,
+    title: 'Непродовольственные товары',
+    iconPath: nonfoodIcon,
+  },
+];
