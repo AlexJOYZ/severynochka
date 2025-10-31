d3f8a3daccf503a672e99c8789b98b1fcf4ce668
AlexJOYZ <zte19062015@gmail.com>
2025-10-31 20:45:38 +0800

    feat(Menu): add hover styles for menu link

--- src/components/UI/menu/Menu.jsx
+++ src/components/UI/menu/Menu.jsx
@@ -12,7 +12,7 @@ export const Menu = forwardRef(function (props, ref) {
   return (
     <menu className={`container ${styles.menu}`} ref={ref}>
       {CATEGORIES.map((item) => (
-        <Link key={item.id} to={`/categories/catalog/${item.id}`}>
+        <Link className={styles.menu__link} key={item.id} to={`/categories/catalog/${item.id}`}>
           <Typography as='h2' variant='text-bold' size='s'>
             {item.title}
           </Typography>
