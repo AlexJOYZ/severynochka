1dc1b7cb8405c8e0522b6e63330b9914982ee145
AlexJOYZ <zte19062015@gmail.com>
2025-11-03 21:02:50 +0800

    feat(/icons/about/*): add new icons for about page

---
+++ src/components/UI/icons/about/RectangleIcon.jsx
@@ -0,0 +1,10 @@
+export const RectangleIcon = () => {
+  return (
+    <svg width='72' height='34' viewBox='0 0 72 34' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M0 9.12752C0 4.08653 4.10643 0 9.17197 0H62.828C67.8936 0 72 4.08653 72 9.12752V24.8725C72 29.9135 67.8936 34 62.828 34H9.17197C4.10643 34 0 29.9135 0 24.8725V9.12752Z'
+        fill='#1CB9FC'
+      />
+    </svg>
+  );
+};
