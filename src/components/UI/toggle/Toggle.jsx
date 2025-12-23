b9e4576c6b6b7912eb7210a3952fcb7b0011bcad
AlexJOYZ <zte19062015@gmail.com>
2025-12-23 18:11:06 +0800

    feat(Toggle): add label

--- src/components/UI/toggle/Toggle.jsx
+++ src/components/UI/toggle/Toggle.jsx
@@ -1,14 +1,21 @@
+import { Typography } from '../Typography/Typography';
+
 import style from './Toggle.module.css';
 
 export const Toggle = ({ size, value, setValue, label, ...props }) => {
   return (
-    <button
-      type='button'
-      className={`${style.toggle__btn} ${style[size]} ${value ? style.toggled : ''}`}
-      onClick={() => setValue(!value)}
-      {...props}
-    >
-      <span className={style.thumb}></span>
-    </button>
+    <label className={style.btn__parent}>
+      <button
+        type='button'
+        className={`${style.toggle__btn} ${style[size]} ${value ? style.toggled : ''}`}
+        onClick={() => setValue(!value)}
+        {...props}
+      >
+        <span className={style.thumb}></span>
+      </button>
+      <Typography as='p' size='s' variant='text'>
+        {label}
+      </Typography>
+    </label>
   );
 };
