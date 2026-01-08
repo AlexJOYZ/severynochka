f7a051a9414e73011770b21dd7faa17e45216dfd
AlexJOYZ <zte19062015@gmail.com>
2026-01-08 17:33:49 +0800

    feat(cartReducer.js): add removeMany items action from Cart

--- src/store/reducers/cartReducer.js
+++ src/store/reducers/cartReducer.js
@@ -6,6 +6,8 @@ const initialCart = {
 
 const ADD_CART = 'ADD_CART';
 const REMOVE_CART = 'REMOVE_CART';
+const REMOVE_MANY_CART = 'REMOVE_MANY_CART';
+const CLEAR_CART = 'CLEAR_CART';
 
 export const cartReducer = (state = initialCart, action) => {
   switch (action.type) {
@@ -13,6 +15,10 @@ export const cartReducer = (state = initialCart, action) => {
       return { ...state, items: [...state.items, action.payload] };
     case REMOVE_CART:
       return { ...state, items: [...removeProductFromCart(state.items, action.payload)] };
+    case REMOVE_MANY_CART:
+      return { ...state, items: state.items.filter((item) => !action.payload.includes(item.id)) };
+    case CLEAR_CART:
+      return {};
     default:
       return state;
   }
@@ -20,3 +26,5 @@ export const cartReducer = (state = initialCart, action) => {
 
 export const addCartAction = (product) => ({ type: ADD_CART, payload: product });
 export const removeCartAction = (id) => ({ type: REMOVE_CART, payload: id });
+export const removeManyCartAction = (mas) => ({ type: REMOVE_MANY_CART, payload: mas });
+export const clearCartAction = () => ({ type: CLEAR_CART });
