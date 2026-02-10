dc21f7e5f7520ee4281556b48dd04fdb2850bb5f
AlexJOYZ <zte19062015@gmail.com>
2026-02-10 20:27:00 +0800

    fix(MainContainer.jsx): add processing className

--- src/components/Layout/MainContainer/MainContainer.jsx
+++ src/components/Layout/MainContainer/MainContainer.jsx
@@ -3,7 +3,7 @@ import { RoutePageHistory } from '../../UI/routePageHistory/RoutePageHistory';
 
 export const MainContainer = ({ routes, children, ...props }) => {
   return (
-    <main className={classNames('container', 'main', props.className)}>
+    <main className={classNames('container', 'main', props.className ? props.className : '')}>
       <RoutePageHistory routes={routes} />
       {children}
     </main>
