d057b134adb72effc03972c956089fa754785eac
AlexJOYZ <zte190620152gmail.com>
2026-03-16 23:10:36 +0800

    feat(Favorites): begin makeup page

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,9 +1,29 @@
 import { MainContainer } from '../components/Layout/MainContainer/MainContainer';
+import { RangeSlider } from '../components/UI/sliders/RangeSlider/RangeSlider';
+import { Typography } from '../components/UI/Typography/Typography';
+
+import '../styles/pages/Favorites.css';
 
 export const Favorites = () => {
   return (
     <MainContainer routes={['Главная', 'Избраное']}>
-      <div>Избраное</div>
+      <Typography as='h1' variant='header' size='xl' className='favorites__header'>
+        Избраное
+      </Typography>
+      <div className='favorites__content'>
+        <div className='favorites__filter__panel'>
+          <Typography
+            as='p'
+            className='favorites__filter__panel__header'
+            variant='text-bold'
+            size='s'
+          >
+            Фильтр
+          </Typography>
+          <RangeSlider step={10} min={0} max={100} />
+        </div>
+        <div className='favorites__products__panel'></div>
+      </div>
     </MainContainer>
   );
 };
