824561e56b21df3d8b53330482e5c9247399f061
AlexJOYZ <zte190620152gmail.com>
2026-06-21 21:34:08 +0800

    fix(RangeSlider): fix max input

--- src/components/UI/sliders/RangeSlider/RangeSlider.jsx
+++ src/components/UI/sliders/RangeSlider/RangeSlider.jsx
@@ -2,58 +2,54 @@ import { useState } from 'react';
 
 import styles from './RangeSlider.module.css';
 
-export const RangeSlider = ({ step, min, max }) => {
-  const [minValue, setMinValue] = useState(min);
-  const [maxValue, setMaxValue] = useState(max);
-
-  const handleMinChange = (event) => {
-    const value = parseFloat(event.target.value);
-    const newMinValue = Math.min(value, maxValue - step);
-    setMinValue(newMinValue);
-    console.log('newMinValue', newMinValue);
+export const RangeSlider = ({ min, max, value, step, onChange }) => {
+  const handleMinChange = (e) => {
+    let newMinVal = Math.min(+e.target.value, value.max - step);
+    if (+e.target.value - min < step) newMinVal = min;
+    onChange({ ...value, min: newMinVal });
   };
 
-  const handleMaxChange = (event) => {
-    const value = parseFloat(event.target.value);
-    const newMaxValue = Math.max(value, minValue + step);
-    setMaxValue(newMaxValue);
-    console.log('newMaxValue', newMaxValue);
+  const handleMaxChange = (e) => {
+    let newMaxVal = Math.max(+e.target.value, value.min + step);
+    if (max - +e.target.value < step) newMaxVal = max;
+    onChange({ ...value, max: newMaxVal });
   };
 
-  const minPosition = ((minValue - min) / (max - min)) * 100;
-  const maxPosition = ((maxValue - min) / (max - min)) * 100;
+  const minPosition = ((value.min - min) / (max - min)) * 100;
+  const maxPosition = ((value.max - min) / (max - min)) * 100;
 
   return (
     <div className={styles.slider__container}>
       <div className={styles.input__wrapper}>
         <input
           className={styles.input}
-          onChange={handleMinChange}
           type='range'
-          value={minValue}
+          value={value.min}
           min={min}
           max={max}
           step={step}
+          onChange={handleMinChange}
         />
         <input
           className={styles.input}
-          onChange={handleMaxChange}
           type='range'
-          value={maxValue}
+          value={value.max}
           min={min}
           max={max}
           step={step}
+          onChange={handleMaxChange}
         />
       </div>
+
       <div className={styles.control__wrapper}>
-        <div className={styles.thumb} style={{ left: `${minPosition}%` }}></div>
+        <div className={styles.thumb} style={{ left: `${minPosition}%` }} />
         <div className={styles.rail}>
           <div
             className={styles.rail__inner}
-            style={{ left: `${minPosition}%`, right: `${maxPosition}%` }}
-          ></div>
+            style={{ left: `${minPosition}%`, right: `${100 - maxPosition}%` }}
+          />
         </div>
-        <div className={styles.thumb} style={{ left: `${maxPosition}%` }}></div>
+        <div className={styles.thumb} style={{ left: `${maxPosition}%` }} />
       </div>
     </div>
   );
